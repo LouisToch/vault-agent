6881536feaad731b3957a0bfc2c024e57c72be47
LouisToch <fzgwyt665v@privaterelay.appleid.com>
2026-06-13 18:54:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -164,5 +164,5 @@ app.get('/orders', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`VAULT Agent running on port ${PORT}`));
